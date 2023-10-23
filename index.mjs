1eaf33c8506f999142eeb9303e4c5e64f96d0e6b
Christian <christianbur1248@gmail.com>
2023-10-23 15:08:00 -0500

    2.0

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
+// Import necessary dependencies
 import Express from 'express';
 import bodyParser from 'body-parser';
 import Boom from '@hapi/boom';
-// Tareas
 import { startConnection } from './src/mongo/index.mjs';
 import FiltersRouter from './src/handlers/filters/index.mjs';
 import { PORT } from './src/commons/env.mjs';
@@ -10,19 +10,34 @@ import buildContainer from './src/container/buildContainer.mjs';
 const app = Express();
 
 app.use(bodyParser.json());
-//
 app.use(buildContainer);
 
-// const upload = multer({ dest: "uploads/" }); // Define la carga de archivos en la carpeta
-
-// Se borro const PORT = 3000;
 app.get('/', (req, res) => {
   res.send('OK');
 });
 
-// Endpoint POST para recibir form data con files[] y filters
-
 app.use('/images', FiltersRouter);
+
+// Define a GET endpoint that retrieves a document by ID
+app.get('/images/:id', async (req, res, next) => {
+  try {
+    const { id } = req.params; // Get the ID from the URL
+    // Replace 'YourModel' with the actual model you're using to interact with MongoDB
+    const document = await YourModel.findById(id);
+
+    if (!document) {
+      // If the document doesn't exist, return an error
+      const error = Boom.notFound('Object not found in MongoDB');
+      return res.status(error.output.statusCode).json(error.output.payload);
+    }
+
+    // If the document exists, return it
+    res.json(document);
+  } catch (error) {
+    // Handle any other errors that may occur
+    next(error);
+  }
+});
 
 app.use((error, req, res, next) => {
   if (error) {
@@ -40,7 +55,6 @@ const startServer = async () => {
   await startConnection();
 
   app.listen(PORT, () => {
-    // eslint-disable-next-line
     console.log(`http://localhost:${PORT}`);
   });
 };
